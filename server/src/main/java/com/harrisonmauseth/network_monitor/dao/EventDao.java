8afa92c49754f31c5ca48470a04c43cbda022035
Harrison Mauseth <arthur.h.mauseth@gmail.com>
2024-09-22 19:28:16 -0500

    created EventDao

--- server/src/main/java/com/harrisonmauseth/network_monitor/dao/EventDao.java
+++ server/src/main/java/com/harrisonmauseth/network_monitor/dao/EventDao.java
@@ -7,27 +7,48 @@ import java.util.List;
 public interface EventDao {
     /**
      * Get all events from the database.
+     *
      * @return a list of all events
      */
     List<Event> getAllEvents();
 
     /**
      * Get a specific event from the database.
+     *
      * @param id the id of the event you wish to retrieve
      * @return the specified event
      */
     Event getEventById(int id);
 
     /**
-     * 
-     * @param eventToCreate
-     * @return
+     * Create a new event.
+     *
+     * @param eventToCreate the event you wish to create
+     * @return the created event that returns from the database
      */
     Event createEvent(Event eventToCreate);
 
+    /**
+     * Create multiple events from an array of events.
+     *
+     * @param events an array of events to create
+     * @return a list of all events that have been created
+     */
     List<Event> createMultipleEvents(Event[] events);
 
+    /**
+     * Update an event.
+     *
+     * @param eventToUpdate the updated event you wish to update in the database
+     * @return the updated event that returns from the database
+     */
     Event updateEvent(Event eventToUpdate);
 
+    /**
+     * Delete an event.
+     *
+     * @param eventId the id of the event to delete
+     * @return the number of lines that were deleted
+     */
     int deleteEvent(int eventId);
 }
